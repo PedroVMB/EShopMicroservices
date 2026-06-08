7cbfc34f645447495bf54c1246d8cda44564c1d3
Pedro Vinicius Mota Barreiro <pedromb.dev@gmail.com>
2026-06-08 13:33:19 -0300

    feat: incluindo endpoints get, getbyid, docker compose e projeto buildingblocks

---
+++ src/Service/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+
+namespace Catalog.API.Products.CreateProduct;
+
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string Imagefile,
+    decimal Price
+) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+
+        var product = new Product
+        {
+            Name = request.Name,
+            Category = request.Category,
+            Description = request.Description,
+            Imagefile = request.Imagefile,
+            Price = request.Price
+        };
+        
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+        
+        return new CreateProductResult(product.Id);
+    }
+}
